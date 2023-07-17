c87e0c784e98684efa0d00d4291001ff3361ad26
Alik Agarwala <alikagarwala@outlook.com>
2023-07-18 00:12:44 +0530

    Refactor code to simplify if conditions

--- fire_alarm_executable.ino
+++ fire_alarm_executable.ino
@@ -12,69 +12,71 @@ const int led = D4;
 const int Buzzer = D6;
 
 char auth[] = BLYNK_AUTH_TOKEN;
-
 char ssid[] = "<WIFI SSID goes here>"; // wifi name
-char pass[] = "<WIFI PASSWORD goes here>";         // wifi password
+char pass[] = "<WIFI PASSWORD goes here>"; // wifi password
 
 BlynkTimer timer;
 
 int fireState = 0;
 int lastfireState = 0;
-unsigned long old = 0;
-unsigned long current = 0;
-int interval = 0;
 
 void Reading()
 {
-    fireState = digitalRead(fireSensor);
-    if (fireState == 0 && lastfireState == 0)
-    {
-        Serial.println("Blynk notification: Fire!");
-        Blynk.logEvent("fire", "Fire Alert");
-        lastfireState = 1;
-        tone(Buzzer, 1000);
-        delay(1000);
-    }
-    if (fireState == 0 && lastfireState == 1)
-    {
-        Serial.println("Fire Alert, Continous");
-        tone(Buzzer, 2000);
-        delay(5000);
-    }
-    if (fireState == 1)
-    {
-        Serial.println("No Fire");
-        lastfireState = 0;
-        noTone(Buzzer);
-    }
+  fireState = digitalRead(fireSensor);
+  
+  if (!fireState && !lastfireState)
+  {
+    Serial.println("Blynk notification: Fire!");
+    Blynk.logEvent("fire", "Fire Alert");
+    lastfireState = 1;
+    tone(Buzzer, 1000);
+    delay(1000);
+  }
+  
+  if (!fireState && lastfireState)
+  {
+    Serial.println("Fire Alert, Continuous");
+    tone(Buzzer, 2000);
+    delay(5000);
+  }
+  
+  if (fireState)
+  {
+    Serial.println("No Fire");
+    lastfireState = 0;
+    noTone(Buzzer);
+  }
 }
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println();
-    pinMode(led, OUTPUT);
-    Serial.println("Please wait.... Sensor activation");
-    delay(1000);
-    Serial.println("Please wait for Blynk Server connection");
-    pinMode(fireSensor, INPUT);
-    pinMode(Buzzer, OUTPUT);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        digitalWrite(led, LOW);
-        delay(250);
-        Serial.print(".");
-        digitalWrite(led, HIGH);
-        delay(250);
-    }
-    Blynk.begin(auth, ssid, pass);
-    timer.setInterval(1000L, Reading);
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(led, OUTPUT);
+  Serial.println("Please wait.... Sensor activation");
+  delay(1000);
+  Serial.println("Please wait for Blynk Server connection");
+  pinMode(fireSensor, INPUT);
+  pinMode(Buzzer, OUTPUT);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(led, LOW);
+    delay(250);
+    Serial.print(".");
+    digitalWrite(led, HIGH);
+    delay(250);
+  }
+  
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, Reading);
 }
 
 void loop()
 {
-    Blynk.run();
-    timer.run();
+  Blynk.run();
+  timer.run();
 }
